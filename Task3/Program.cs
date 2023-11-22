7533726e78cde0c4e6bde50ad14328d9e139506f
mamykin <mamykin_87@mail.ru>
2023-11-22 19:32:00 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+int[] array = GetArray(10, 0, 10);
+Console.WriteLine(string.Join(" ", array));
+
+int[] reverseArray = ReversArray1(array);
+Console.WriteLine(string.Join(" ", reverseArray));
+
+// ReversArray2(array);                               // второй вариант решения
+// Console.WriteLine(string.Join(" ", array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] ReversArray1(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for(int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
